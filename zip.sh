8efb2a5b210ca078c2a6e618a65d819e978fdfca
katonori <katonori.d@gmail.com>
2014-11-24 16:57:18 +0900

    ver 1.0.1

--- zip.sh
+++ zip.sh
@@ -1,2 +1,2 @@
-zip a.xpi LICENSE.txt README.mkd bootstrap.js chrome.manifest content/ icon.png icon64.png install.rdf locale/
+zip a.xpi Changelog.txt LICENSE.txt README.mkd bootstrap.js chrome.manifest content/ icon.png icon64.png install.rdf locale/
 cp a.xpi /var/www/html/
